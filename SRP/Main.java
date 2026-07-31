38d9381a67a598ba2c83ca27ce9a1b94b624c2c5
Ashish Kumar Mishra <amishrahsm@gmail.com>
2026-07-31 14:58:12 +0530

    fixed single Responsibility

--- SRP/Main.java
+++ SRP/Main.java
@@ -1,13 +1,21 @@
 package SRP;
 import java.util.*;
-class Main{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        ShoppingCart cart = new ShoppingCart();
-        cart.addProduct(new Product("Laptop", 1500));
-        cart.addProduct(new Product("Mouse",50));
+public class Main {
 
-        cart.printInvoice();
-        cart.saveToDatabase();
+    public static void main(String[] args) {
+
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addProduct(new Product("Laptop", 1500));
+        cart.addProduct(new Product("Mouse", 50));
+
+        CartCalculator calculator = new CartCalculator();
+        double total = calculator.calculatePrice(cart.getProducts());
+
+        CartInvoicePrinter printer = new CartInvoicePrinter();
+        printer.printInvoice(cart.getProducts(), total);
+
+        CartDBStorage db = new CartDBStorage();
+        db.saveToDatabase(cart);
     }
 }
--- SRP/Product.java
+++ SRP/Product.java
@@ -12,37 +12,96 @@ public class Product {
     }
 }
 // voilating SRP : shopping is handling multiple responsibilities
-class ShoppingCart{
-    private List<Product>products = new ArrayList<>();
 
-    public void addProduct(Product p){
+// class ShoppingCart{
+//     private List<Product>products = new ArrayList<>();
+
+//     public void addProduct(Product p){
+//         products.add(p);
+//     }
+
+//     public List<Product>getProducts(){
+//         return produts;
+//     }
+
+//     public double calculateTotal(){
+//         double total = 0;
+//         for(int i=0;i<products.size();i++){
+//             Product a = products.get(i);
+//             total += a.price;
+//         }
+//         return total;
+//     }
+
+//     public void printInvoice(){
+//         System.out.println("Shopping Cart Invoice: ");
+        
+//         for(int i=0;i<products.size();i++){
+//             System.out.println(products.get(i).name+" - ₹ "+products.get(i).price);
+//         }
+//         System.out.println("Total: ₹ "+calculateTotal());
+        
+//     }
+
+//     void saveToDatabase(){
+//         System.out.println("Saving shopping cart to Database....");
+//     }
+// }
+
+
+
+// Shopping cart -> only manages products
+class ShoppingCart {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p) {
         products.add(p);
     }
 
-    public List<Product>getProducts(){
-        return produts;
-    }
-
-    public double calculateTotal(){
-        double total = 0;
-        for(int i=0;i<products.size();i++){
-            Product a = products.get(i);
-            total += a.price;
-        }
-        return total;
-    }
-
-    public void printInvoice(){
-        System.out.println("Shopping Cart Invoice: ");
-        
-        for(int i=0;i<products.size();i++){
-            System.out.println(products.get(i).name+" - ₹ "+products.get(i).price);
-        }
-        System.out.println("Total: ₹ "+calculateTotal());
-        
-    }
-
-    void saveToDatabase(){
-        System.out.println("Saving shopping cart to Database....");
+    public List<Product> getProducts() {
+        return products;
     }
 }
+
+// Responsible only for price calculation
+class CartCalculator {
+
+    public double calculatePrice(List<Product> products) {
+
+        double total = 0;
+
+        for (Product p : products) {
+            total += p.price;
+        }
+
+        return total;
+    }
+}
+
+// Responsible only for printing invoice
+class CartInvoicePrinter {
+
+    public void printInvoice(List<Product> products, double total) {
+
+        System.out.println("Shopping Cart Invoice");
+
+        for (Product p : products) {
+            System.out.println(p.name + " - ₹ " + p.price);
+        }
+
+        System.out.println("Total = ₹ " + total);
+    }
+}
+
+
+
+// Responsible only for database storage
+class CartDBStorage {
+
+    public void saveToDatabase(ShoppingCart cart) {
+        System.out.println("Saving shopping cart to database...");
+    }
+}
+
+
